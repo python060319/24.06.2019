f2a51598218a8f763e0bbc6d54bb82905552560b
python060319 <51337287+python060319@users.noreply.github.com>
2019-06-24 20:00:35 +0300

    Update function_break.py

--- function_break.py
+++ function_break.py
@@ -22,4 +22,25 @@ if number % 2 != 0:
 else:
     print('successfully completed...')
     
-        
+
+dishes = ['burger', 'ice-cream', 'cracker', 'coffee']
+for dish in dishes:
+    print(dish)
+    for letter in dish: # ['b','u','r','g','e','r']
+        print(letter)
+
+matrix = [[10, 22, 36], [-4, 25, 63], [87, 98, 109]]
+# print sum of each list
+# find biggest number
+#matrix[1][2]
+
+biggest = matrix[0][0]
+for row in matrix:
+    sum = 0
+    for col in row:
+        sum = sum + col
+        if col > biggest:
+            biggest = col
+    print(f'{row} sum is {sum}')
+print(f'the bigegst number is {biggest}')
+
